7da5dc515e0e6a2520ec30a925b898cb84925310
lanceplarsen <lance@hashicorp.com>
2021-08-24 09:24:05 -0500

    add kube

--- deployments/toplogies/mixed/vm-primary/secondaries/kube/setup.sh
+++ deployments/toplogies/mixed/vm-primary/secondaries/kube/setup.sh
@@ -2,7 +2,7 @@
 
 kubectl create secret generic consul-federation \
     --from-literal=caCert="$(cat certs/consul-agent-ca.pem)" \
-    --from-literal=caKey="$(cat certs/consul-agent-ca.key)"
+    --from-literal=caKey="$(cat certs/consul-agent-ca.key)" \
     --from-literal=replicationToken="<your acl replication token>" \
     --from-literal=gossipEncryptionKey="<your gossip encryption key>"
 
